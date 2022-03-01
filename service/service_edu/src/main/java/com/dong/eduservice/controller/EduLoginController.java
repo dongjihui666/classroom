78d042056d8cccb097f779a19141f10d98d8e6b1
dongjihui666 <dongjihui0320@gmail.com>
2022-03-01 21:30:01 +0900

    创建了登录接口功能

---
+++ service/service_edu/src/main/java/com/dong/eduservice/controller/EduLoginController.java
@@ -0,0 +1,21 @@
+package com.dong.eduservice.controller;
+
+import com.dong.commonutils.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/eduservice/user")
+public class EduLoginController {
+
+    @PostMapping("login")
+    private R Login(){
+        return R.ok().data("token","admin");
+    }
+    @GetMapping("info")
+    private R info(){
+        return R.ok().data("roles","[admin]").data("name","admin").data("avatar","https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif");
+    }
+}
